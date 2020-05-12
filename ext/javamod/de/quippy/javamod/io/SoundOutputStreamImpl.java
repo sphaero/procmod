bb35bf19f069b314040126b6133adeb6eeba9cfa
Arnaud Loonstra <arnaud@sphaero.org>
2020-05-12 11:08:34 +0200

    raise buffer to 4096 to prevent blocking :S

--- ext/javamod/de/quippy/javamod/io/SoundOutputStreamImpl.java
+++ ext/javamod/de/quippy/javamod/io/SoundOutputStreamImpl.java
@@ -92,7 +92,7 @@ public class SoundOutputStreamImpl implements SoundOutputStream
 					sourceLineInfo.getFormats();
 					sourceLine = (SourceDataLine) AudioSystem.getLine(sourceLineInfo);
 					sourceLine.getFormat();
-					sourceLine.open(sourceLine.getFormat(), 2200);
+					sourceLine.open(sourceLine.getFormat(), 4096);
 					sourceLine.start();
 					setVolume(currentVolume);
 					setBalance(currentBalance);
